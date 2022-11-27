3599ba150981a9284aa7bf19abf52dc6ff779467
leon <lswx0925@gmail.com>
2022-11-27 22:22:19 +0800

    chore: init

---
+++ src/views/Home.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <h1>欢迎访问本系统！</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      tableData: []
+    };
+  },
+  created() {
+    this.query();
+  }
+};
+</script>
+
+<style scoped>
+.home-wrap {
+  height: 100%;
+  width: 100%;
+}
+
+.main-wrap {
+  margin-top: 10px;
+  height: 100%;
+  width: 100%;
+  background-color: #fff;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+
+.home-container {
+  height: 100%;
+  width: 90%;
+  margin: auto;
+}
+
+.home-aside {
+  height: 100%;
+}
+
+.aside-container {
+  width: 90%;
+  margin-left: auto;
+  margin-right: auto;
+  height: 200px;
+  margin-top: 20px;
+}
+
+.aside-img {
+  width: 100%;
+}
+
+.aside-title {
+  color: #333;
+  font-size: 18px;
+}
+
+.aside-content {
+  font-size: 12px;
+  color: #999;
+}
+</style>
